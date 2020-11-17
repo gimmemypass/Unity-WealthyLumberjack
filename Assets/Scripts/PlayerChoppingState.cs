d6da206cf28ae61732c17cdf663e9fdba7db6d88
inty <dgolovenko01@gmail.com>
2020-11-18 01:08:23 +0300

    first commit

---
+++ Assets/Scripts/PlayerChoppingState.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(order = 51, menuName = "Scriptable Objects/PlayerChoppingState")]
+public class PlayerChoppingState : PlayerState
+{
+    #region Data
+    [SerializeField] private float _turnSmoothTime = 0.1f;
+    [SerializeField] private LayerMask treeLayer;
+    [SerializeField] private float choppingRange = 1f;
+
+    private float _turnSmoothSpeed;
+    private Animator _animator;
+    private int _speedAnimationHash;
+    private float _percentsOfSpeed;
+    private Collider[] _trees;
+    #endregion
+
+    #region Interface
+    public override void Init(Player player)
+    {
+        base.Init(player);
+        _animator = player.GetComponent<Animator>();
+        _speedAnimationHash = Animator.StringToHash("Speed");
+        _trees = new Collider[0];
+        _player.StartCoroutine(ChopTrees());
+    }
+    public override void Run()
+    {
+        var direction = _player.GetJoystickInputValue();
+        _percentsOfSpeed = direction.magnitude;
+        if (_percentsOfSpeed >= 0.3f)
+        {
+            _animator.SetFloat(_speedAnimationHash, _percentsOfSpeed);
+            IsFinished = true;
+            return;
+        }
+
+    }
+    #endregion
+
+    #region Methods
+    private IEnumerator ChopTrees()
+    {
+        _trees = Physics.OverlapSphere(_player.transform.position, choppingRange, treeLayer);
+        while (IsFinished == false && _trees.Length > 0)
+        {
+            _animator.SetTrigger("Chopping");
+            Debug.Log("chopping");
+            yield return new WaitForSeconds(1);
+            foreach(var tree in _trees)
+            {
+                tree.gameObject.SetActive(false);
+            }
+            _trees = Physics.OverlapSphere(_player.transform.position, choppingRange, treeLayer);
+            yield return new WaitForSeconds(1);
+        }
+    }
+    #endregion
+
+}
---
+++ Assets/Scripts/PlayerMovementState.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(order = 51, menuName = "Scriptable Objects/PlayerMovementState")]
+public class PlayerMovementState : PlayerState
+{
+    #region Data
+    [SerializeField] private float _speed = 6f;
+    [SerializeField] private float _turnSmoothTime = 0.1f;
+    
+    private CharacterController _controller;
+    private Animator _animator;
+    private float _turnSmoothSpeed;
+    private int _speedAnimationHash;
+    private float _percentsOfSpeed;
+
+    #endregion
+
+    #region Interface
+    public override void Init(Player player)
+    {
+        base.Init(player);
+        _controller = player.GetComponent<CharacterController>();
+        _animator = player.GetComponent<Animator>();
+        _speedAnimationHash = Animator.StringToHash("Speed");
+        
+    }
+    public override void Run()
+    {
+        var direction = _player.GetJoystickInputValue();
+        _percentsOfSpeed = direction.magnitude;
+        if(_percentsOfSpeed >= 0.3f)
+        {
+            MovePlayer(direction);
+        }
+        else
+        {
+            _animator.SetFloat(_speedAnimationHash, 0);
+            IsFinished = true;
+        }
+    }
+
+    #endregion
+
+    #region Methods
+    private void MovePlayer(Vector3 direction)
+    {
+        _animator.SetFloat(_speedAnimationHash, _percentsOfSpeed);
+        //Debug.Log($"direction magnitude is {_percentsOfSpeed}");
+        float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+        float angle = Mathf.SmoothDampAngle(_player.transform.eulerAngles.y, targetAngle, ref _turnSmoothSpeed, _turnSmoothTime);
+        _player.transform.rotation = Quaternion.Euler(0, angle, 0);
+        _controller.Move(direction * (_speed * _percentsOfSpeed) * Time.deltaTime);
+
+    }
+    #endregion
+}
